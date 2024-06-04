b11a6a2c3ca836e13bbfe9d25f0cf748aa9a5ff3
AiYuKai <yuukitamura1219@gmail.com>
2024-06-04 10:37:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -58,10 +58,7 @@ def main_page():
             st.write(speaker + ": " + message["content"])
 # streamlitページの構築
 authenticator = stauth.Authenticate(
-    credentials=config['credentials'],
-    cookie_name=config['cookie']['name'],
-    cookie_key=config['cookie']['key'],
-    cookie_expiry_days=config['cookie']['expiry_days'],
+    
 )
 
 ## UI 
